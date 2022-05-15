f7d953839d529736de198d21e5b7c2ba0e62e6e8
deecooper <deecoopermedia@gmail.com>
2022-05-15 15:26:28 +0000

    Add function for player choice

---
+++ assests/js/script.js
@@ -0,0 +1,12 @@
+let computerChoice = document.getElementById("computer-choice");
+let playerChoice = document.getElementById("player-choice");
+let score = document.getElementById("score");
+let choices = document.querySelectorAll("button");
+let playerSelection;
+
+choices.forEach(choice => choice.addEventListener("click", (e) => {
+    playerSelection = e.target.id;
+    playerChoice.innerHTML = playerSelection;
+
+}
+));
