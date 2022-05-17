998abb48c2128f00d99a2d792fbc7c0f31eb84ec
deecooper <deecoopermedia@gmail.com>
2022-05-17 22:27:27 +0000

    Add computer choice function and get result

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,16 +1,81 @@
-let computerChoice = document.getElementById("computer-choice");
+let computerChoiceDisplay = document.getElementById("computer-choice");
 let playerChoice = document.getElementById("player-choice");
-let score = document.getElementById("score");
+let result = document.getElementById("score");
 let choices = document.querySelectorAll("button");
 let playerSelection;
+let computerChoice;
+let score = 0;
+
+let gameIcons = {
+    rock: "./images/rock.png",
+    scissors: "./images/scissors.png",
+    paper: "./images/paper.png"
+};
 
 choices.forEach(choice => choice.addEventListener("click", (e) => {
     playerSelection = e.target.id;
     playerChoice.innerHTML = playerSelection;
+    computerSelection()
 
 }
 ));
 
+
 function computerSelection() {
-    let randomNumber = Math.random() * 3
+    let randomNumber = Math.floor(Math.random() * 3) + 1
+    console.log(randomNumber)
+
+    if(randomNumber === 1){
+        computerChoice = "rock";
+    }
+    else if(randomNumber === 2){
+        computerChoice = "paper";
+
+    }
+    else if (randomNumber === 3){
+        computerChoice = "scissors";
+
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (user === "rock"){
+        if (computerChoice === "rock"){
+            return "Its a draw";
+        } else if (computerChoice === "scissors"){
+            return "You Won";
+            score ++;
+        } else if (computerChoice === "paper") {
+            return "You lose";
+      
+        }
+
+    }
+
+        if (user === "paper"){
+            if (computerChoice === "paper"){
+                return "Its a draw";
+            } else if (computerChoice === "rock"){
+                return "You Won";
+                score ++;
+            } else if (computerChoice === "scissors") {
+                return "You lose";
+          
+            }
+        }
+
+        if (user === "scissors"){
+            if (computerChoice === "scissors"){
+                return "Its a draw";
+            } else if (computerChoice === "paper"){
+                return "You Won";
+                score ++;
+            } else if (computerChoice === "rock") {
+                return "You lose";
+          
+            }
+        }
+ 
+
 }
